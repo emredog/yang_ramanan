114bef238008c53e0696db99cfb86bb99ea756ae
emredog <emredog@gmail.com>
2015-02-16 11:24:40 +0100

    Divide big images into 4 patches and run bpe algorithm

--- pose-release-FULL-ver1.3/code-basic/demo.m
+++ pose-release-FULL-ver1.3/code-basic/demo.m
@@ -1,8 +1,8 @@
 addpath visualization;
 if isunix()
-  addpath mex_unix;
+    addpath mex_unix;
 elseif ispc()
-  addpath mex_pc;
+    addpath mex_pc;
 end
 
 compile;
@@ -11,11 +11,11 @@ compile;
 load('PARSE_model');
 visualizemodel(model);
 disp('model template visualization');
-disp('press any key to continue'); 
+disp('press any key to continue');
 pause;
 visualizeskeleton(model);
 disp('model tree visualization');
-disp('press any key to continue'); 
+disp('press any key to continue');
 pause;
 
 imlist = dir('images/*.jpg');
@@ -23,23 +23,52 @@ for i = 1:length(imlist)
     % load and display image
     im = imread(['images/' imlist(i).name]);
     clf; imagesc(im); axis image; axis off; drawnow;
-
-    % call detect function
-    tic;
-    boxes = detect_fast(im, model, min(model.thresh,-1));
-    dettime = toc; % record cpu time
-    if isempty(boxes)
-        fprintf('No detection after %.1f seconds\n',dettime);
+    
+    [h, w, channels] = size(im);
+    if h == 240
+        
+        % call detect function
+        tic;
+        boxes = detect_fast(im, model, min(model.thresh,-1));
+        dettime = toc; % record cpu time
+        if isempty(boxes)
+            fprintf('No detection after %.1f seconds\n',dettime);
+        else
+            boxes = nms(boxes, .1); % nonmaximal suppression
+            colorset = {'g','g','y','m','m','m','m','y','y','y','r','r','r','r','y','c','c','c','c','y','y','y','b','b','b','b'};
+            showboxes(im, boxes(1,:),colorset); % show the best detection
+            %showboxes(im, boxes,colorset);  % show all detections
+            fprintf('detection took %.1f seconds\n',dettime);
+            %disp('press any key to continue');
+        end
+        saveas(gcf, ['images/result_' imlist(i).name] ,'jpg');
+        %pause;
     else
-        boxes = nms(boxes, .1); % nonmaximal suppression
-        colorset = {'g','g','y','m','m','m','m','y','y','y','r','r','r','r','y','c','c','c','c','y','y','y','b','b','b','b'};
-        showboxes(im, boxes(1,:),colorset); % show the best detection
-        %showboxes(im, boxes,colorset);  % show all detections
-        fprintf('detection took %.1f seconds\n',dettime);
-        disp('press any key to continue');        
-    end 
-    saveas(gcf, ['images/result_' imlist(i).name] ,'jpg');
-    pause;
+        % assumption: h=480
+        for x = 0:1
+            for y = 0:1
+                imCropped = imcrop(im, [x*320 y*240 320 240]);
+                % call detect function
+                tic;
+                boxes = detect_fast(imCropped, model, min(model.thresh,-1));
+                dettime = toc; % record cpu time
+                if isempty(boxes)
+                    fprintf('No detection after %.1f seconds\n',dettime);
+                    %disp('press any key to continue');
+                else
+                    boxes = nms(boxes, .1); % nonmaximal suppression
+                    colorset = {'g','g','y','m','m','m','m','y','y','y','r','r','r','r','y','c','c','c','c','y','y','y','b','b','b','b'};
+                    showboxes(imCropped, boxes(1,:),colorset); % show the best detection
+                    %showboxes(im, boxes,colorset);  % show all detections
+                    fprintf('detection took %.1f seconds\n',dettime);
+                    %disp('press any key to continue');
+                end
+                saveas(gcf, ['images/result_' imlist(i).name(1:end-4) '_' num2str(x) '-' num2str(y) '.jpg'] ,'jpg');
+                %pause;
+            end
+        end
+    end
+    
 end
 
 disp('done');
