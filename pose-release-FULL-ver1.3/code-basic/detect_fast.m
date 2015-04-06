568ea3b41b2f52999845806583e172d1d7b33c3f
emredog <emredog>
2015-04-06 15:03:48 +0300

    Backtrack now returns all part scores

--- pose-release-FULL-ver1.3/code-basic/detect_fast.m
+++ pose-release-FULL-ver1.3/code-basic/detect_fast.m
@@ -10,7 +10,7 @@ function boxes = detect_fast(im, model, thresh)
 
 % Compute the feature pyramid and prepare filter
 tic;
-pyra     = featpyramid(im,model);
+pyra     = featpyramid(im,model); %ED - Calculates HOG features for different scales
 pyratime = toc;
 fprintf('Pyramid constructed in %.3f seconds\n',pyratime);
 interval = model.interval;
@@ -33,7 +33,7 @@ for rlevel = levels,
       f     = parts(k).filterid;
       level = rlevel-parts(k).scale*interval;
       if isempty(resp{level}),
-        resp{level} = fconv(pyra.feat{level},filters,1,length(filters));
+        resp{level} = fconv(pyra.feat{level},filters,1,length(filters)); %ED - Calculates HOG responses of pyramid against the trained model.
       end
       for fi = 1:length(f)
         parts(k).score(:,:,fi) = resp{level}{f(fi)};
@@ -47,22 +47,24 @@ for rlevel = levels,
     % Walk from leaves to root of tree, passing message to parent
     for k = numparts:-1:2,
       par = parts(k).parent;
-      [msg,parts(k).Ix,parts(k).Iy,parts(k).Ik] = passmsg(parts(k),parts(par));
-      parts(par).score = parts(par).score + msg;
+      [msg,parts(k).Ix,parts(k).Iy,parts(k).Ik] = passmsg(parts(k),parts(par)); %ED - Calculate deformation score between k and k-1(=par, parent of k) --> msg
+                                                                                %ED - k.Ix and k.Iy contains, for each (pi,ti) of k, potential pi of k-1
+                                                                                %ED - k.Ik contains, for each (pi,ti) of k, potential ti of k-1
+      parts(par).score = parts(par).score + msg; %ED - score of PARENT is updated with msg (deformation score between k and k-1(=par, parent of k))
     end
     %passmsgTime = toc;
     %fprintf('Msg  for level: %d component %d in %.3f seconds\n',rlevel, c, passmsgTime);
 
     % Add bias to root score
-    parts(1).score = parts(1).score + parts(1).b;
-    [rscore Ik]    = max(parts(1).score,[],3);
+    parts(1).score = parts(1).score + parts(1).b; %ED - Bias score is from model components, ie, a learned value
+    [rscore Ik]    = max(parts(1).score,[],3);    %ED - For the root part, select best ti for every pi, based on obtained score.
 
     % Walk back down tree following pointers
     %thresh = -100.0;
-    [Y,X] = find(rscore >= thresh); %finds indices of rscore elements that are >= threshold
+    [Y,X] = find(rscore >= thresh); %ED - finds indices of rscore elements that are >= threshold, let's say there are 250 candidates:
     if length(X) > 1,
       I   = (X-1)*size(rscore,1) + Y;
-      box = backtrack(X,Y,Ik(I),parts,pyra);
+      [box, scores] = backtrack(X,Y,Ik(I),parts,pyra); %ED - this function backtrack a single root candidate until it reaches the leaf, 
       i   = cnt+1:cnt+length(I);
       boxes(i,:) = [box repmat(c,length(I),1) rscore(I)];
       cnt = i(end);
@@ -137,6 +139,8 @@ function [score,Ix,Iy,Ik] = passmsg(child,parent)
   for k = 1:K
     [score0(:,:,k),Ix0(:,:,k),Iy0(:,:,k)] = shiftdt(child.score(:,:,k), child.w(1,k), child.w(2,k), child.w(3,k), child.w(4,k),child.startx(k),child.starty(k),Nx,Ny,child.step);
   end
+  %ED - shiftdt makes -some- distance transform to rapidly calculate the
+  %score related to distance [dx dx^2 dy dy^2] = ψ(p_child, p_parent)
   
 %  csvwrite('inp_score0.csv', score0);
 %  csvwrite('inp_Ix0.csv', Ix0);
@@ -169,7 +173,8 @@ function [score,Ix,Iy,Ik] = passmsg(child,parent)
 %  csvwrite('out_Ik.csv', Ik);
 
 % Backtrack through DP msgs to collect ptrs to part locations
-function box = backtrack(x,y,mix,parts,pyra)
+%ED - DP:dynamic programming
+function [box, scores] = backtrack(x,y,mix,parts,pyra)
   numx     = length(x);
   numparts = length(parts);
   
@@ -177,21 +182,28 @@ function box = backtrack(x,y,mix,parts,pyra)
   yptr = zeros(numx,numparts);
   mptr = zeros(numx,numparts);
   box  = zeros(numx,4,numparts);
+  %ED copy the scores of each part for every candidate:
+  scores = zeros(numx,numparts);
 
   for k = 1:numparts,
     curPart   = parts(k);
-    if k == 1, %If current part is root:
+    if k == 1, %ED - If current part is root:
       xptr(:,k) = x;
       yptr(:,k) = y;
       mptr(:,k) = mix;
-    else %if current part is any other part than root
+      [h,w,foo] = size(parts(k+1).Ix); %ED - get patch size
+      Icur = (mix-1)*h*w + (x-1)*h + y; %ED - fetch indices of best scoring pi for current part
+      scores(:,k) = curPart.score(Icur); %ED - fetch best scores for current part
+    else %ED - if current part is any other part than root
       % I = sub2ind(size(p.Ix),yptr(:,par),xptr(:,par),mptr(:,par));
       curParent = curPart.parent;
       [h,w,foo] = size(curPart.Ix);
-      I   = (mptr(:,curParent)-1)*h*w + (xptr(:,curParent)-1)*h + yptr(:,curParent);
+      I   = (mptr(:,curParent)-1)*h*w + (xptr(:,curParent)-1)*h + yptr(:,curParent);      
       xptr(:,k) = curPart.Ix(I);
       yptr(:,k) = curPart.Iy(I);
       mptr(:,k) = curPart.Ik(I);
+      Icur = (mptr(:,k)-1)*h*w + (xptr(:,k)-1)*h + yptr(:,k); %ED - fetch indices of best scoring pi for current part
+      scores(:,k) = curPart.score(Icur); %ED - fetch best scores for current part
     end
     scale = pyra.scale(curPart.level);
     x1 = (xptr(:,k) - 1 - pyra.padx)*scale+1;
